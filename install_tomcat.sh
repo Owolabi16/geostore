e88382c7ce9eaa7d5fd2a9f57cfb83f157515a69
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-19 02:11:24 +0100

    updated sheel script

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
-# Install Tomcat
+set -e
+
+# Variables
 TOMCAT_VERSION=9.0.56
-cd /opt
+TOMCAT_DIR=/opt/tomcat
+TOMCAT_USER=tomcat
+
+# Update package list and install dependencies
+sudo apt update -y
+sudo apt install -y openjdk-8-jdk wget
+
+# Create Tomcat user
+sudo useradd -m -U -d $TOMCAT_DIR -s /bin/false $TOMCAT_USER
+
+# Download and install Tomcat
+cd /tmp
 wget https://downloads.apache.org/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
-tar -xzvf apache-tomcat-$TOMCAT_VERSION.tar.gz
-ln -s apache-tomcat-$TOMCAT_VERSION tomcat
-chown -R ec2-user:ec2-user /opt/apache-tomcat-$TOMCAT_VERSION
+sudo mkdir -p $TOMCAT_DIR
+sudo tar -xzf apache-tomcat-$TOMCAT_VERSION.tar.gz -C $TOMCAT_DIR --strip-components=1
+sudo chown -R $TOMCAT_USER:$TOMCAT_USER $TOMCAT_DIR
+
+# Set up Tomcat as a systemd service
+sudo bash -c 'cat << EOF > /etc/systemd/system/tomcat.service
+[Unit]
+Description=Apache Tomcat Web Application Container
+After=network.target
+
+[Service]
+Type=forking
+
+User=tomcat
+Group=tomcat
+
+Environment="JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre"
+Environment="CATALINA_PID=/opt/tomcat/temp/tomcat.pid"
+Environment="CATALINA_HOME=/opt/tomcat"
+Environment="CATALINA_BASE=/opt/tomcat"
+Environment="CATALINA_OPTS=-Xms512M -Xmx1024M -server -XX:+UseParallelGC"
+Environment="JAVA_OPTS=-Djava.awt.headless=true -Djava.security.egd=file:/dev/./urandom"
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF'
+
+# Reload systemd daemon
+sudo systemctl daemon-reload
+
+# Start and enable Tomcat service
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
