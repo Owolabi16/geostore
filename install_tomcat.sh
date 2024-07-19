73282d9f0fae3c9a173454644eb6507a9295fd49
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-19 02:04:05 +0100

    updated sheel script

--- install_tomcat.sh
+++ install_tomcat.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 # Install Tomcat
-sudo apt install -y tomcat
+TOMCAT_VERSION=9.0.56
+cd /opt
+wget https://downloads.apache.org/tomcat/tomcat-9/v$TOMCAT_VERSION/bin/apache-tomcat-$TOMCAT_VERSION.tar.gz
+tar -xzvf apache-tomcat-$TOMCAT_VERSION.tar.gz
+ln -s apache-tomcat-$TOMCAT_VERSION tomcat
+chown -R ec2-user:ec2-user /opt/apache-tomcat-$TOMCAT_VERSION
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
