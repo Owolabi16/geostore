a3b97c56decc349eef114ebe04c701184953f05b
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-21 18:11:13 +0100

    new build

--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -10,3 +10,15 @@ else
   echo "Failed to stop Tomcat."
   exit 1
 fi
+
+#!/bin/bash
+
+# Navigate to the deployment archive directory
+cd /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
+
+# Unzip the deployment.zip file
+unzip deployment.zip -d /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
+
+# Verify the contents of the unzipped directory
+ls -l /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
+echo "new build"
