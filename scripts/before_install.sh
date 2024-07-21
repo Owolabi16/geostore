7c01d1a871af5a54fd7ad5daebc477e193cae8a2
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-21 17:50:59 +0100

    anything

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
 # Navigate to the deployment archive directory
-cd /opt/codedeploy-agent/deployment-root/$(ls /opt/codedeploy-agent/deployment-root)/d-*/deployment-archive
+cd /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
 
 # Unzip the deployment.zip file
 unzip deployment.zip -d /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
 
 # Verify the contents of the unzipped directory
-ls -l /opt/codedeploy-agent/deployment-root/$(ls /opt/codedeploy-agent/deployment-root)/d-*/deployment-archive
+ls -l /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
+
+#!/bin/bash
+
+# # Find the latest deployment directory
+# DEPLOYMENT_DIR=$(find /opt/codedeploy-agent/deployment-root -mindepth 2 -maxdepth 2 -type d | sort | tail -n 1)
+
+# # Unzip the deployment.zip file
+# unzip $DEPLOYMENT_DIR/deployment.zip -d $DEPLOYMENT_DIR
+
+# # Verify the contents of the unzipped directory
+# ls -l $DEPLOYMENT_DIR
+
