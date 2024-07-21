5eb14323018cc125498ccc6747a6f2d53d7da110
Aliu Ede <68362235+Kasala99@users.noreply.github.com>
2024-07-21 17:37:07 +0100

    hopefully this works

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,7 +4,7 @@
 cd /opt/codedeploy-agent/deployment-root/$(ls /opt/codedeploy-agent/deployment-root)/d-*/deployment-archive
 
 # Unzip the deployment.zip file
-unzip deployment.zip -d /opt/codedeploy-agent/deployment-root/$(ls /opt/codedeploy-agent/deployment-root)/d-*/deployment-archive
+unzip deployment.zip -d /opt/codedeploy-agent/deployment-root/*/d-*/deployment-archive
 
 # Verify the contents of the unzipped directory
 ls -l /opt/codedeploy-agent/deployment-root/$(ls /opt/codedeploy-agent/deployment-root)/d-*/deployment-archive
